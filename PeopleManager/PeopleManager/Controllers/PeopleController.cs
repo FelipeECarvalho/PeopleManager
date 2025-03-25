d64cea74989916c0313c1222157972e70e301429
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-03-24 22:41:25 -0300

    feat: showing people name on index

--- PeopleManager/PeopleManager/Controllers/PeopleController.cs
+++ PeopleManager/PeopleManager/Controllers/PeopleController.cs
@@ -5,13 +5,26 @@ namespace PeopleManager.Controllers
 {
     public class PeopleController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string name)
         {
-            return View();
+            var personList = new List<Person>()
+            {
+                new() { Name = "John Doe", Age = 14 },
+                new() { Name = "Marcus", Age = 15 },
+                new() { Name = "Felipe", Age = 20 },
+                new() { Name = "Test", Age = 23 }
+            };
+
+            var person = personList.FirstOrDefault(p => p.Name == name);
+
+            return View(person);
         }
 
         public IActionResult Details()
         {
+            ViewBag.Date = DateTime.Now.ToShortDateString();
+            ViewBag.Text = "Welcome!";
+
             var personList = new List<Person>()
             {
                 new() { Name = "John Doe", Age = 14 },
