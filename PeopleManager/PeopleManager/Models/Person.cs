12cca524cbaff0c1e90c5100dd252cfeccdfa5b4
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-03-24 23:10:10 -0300

    refactor: new validations

--- PeopleManager/PeopleManager/Models/Person.cs
+++ PeopleManager/PeopleManager/Models/Person.cs
@@ -7,8 +7,20 @@ namespace PeopleManager.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "O nome deve ser maior que 2 e menor que 10 caracteres")]
         public string Name { get; set; }
 
+        [CustomValidation(typeof(Person), "AgeValidate")]
         public int? Age { get; set; }
+
+        public static ValidationResult AgeValidate(int? age)
+        {
+            if (age < 18)
+            {
+                return new ValidationResult("A idade deve ser maior que 18 anos");
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
