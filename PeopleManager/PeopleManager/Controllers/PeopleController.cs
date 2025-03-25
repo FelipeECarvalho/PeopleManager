b6a865c0aadfc0c8288a2bc4628ee968d61d367a
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-03-24 22:10:43 -0300

    feat: people controller added

---
+++ PeopleManager/PeopleManager/Controllers/PeopleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeopleManager.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(id);
+        }
+    }
+}
