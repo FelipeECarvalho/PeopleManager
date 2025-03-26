0501e361dcbd9231c6c4df62eb556946a07a5d8b
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-03-25 21:36:47 -0300

    feat: better project organization

--- PeopleManager/Model/Person.cs
+++ PeopleManager/Model/Person.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace PeopleManager.Models
+namespace Model
 {
     public class Person
     {
--- PeopleManager/PeopleManager/Controllers/PeopleController.cs
+++ PeopleManager/PeopleManager/Controllers/PeopleController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using PeopleManager.Models;
+using Model;
 
 namespace PeopleManager.Controllers
 {
