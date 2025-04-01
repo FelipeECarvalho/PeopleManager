b725a44de2a7d4bbd971292391af7ec1eb2c7c2b
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-03-31 21:53:25 -0300

    Adding images to the front page

--- PeopleManager/PeopleManager.API/Controllers/HomeController.cs
+++ PeopleManager/PeopleManager.API/Controllers/HomeController.cs
@@ -1,22 +1,34 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PeopleManager.Application.ViewModels;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace PeopleManager.API.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _env = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var filePath = Path.Combine(_env.WebRootPath, "images");
+
+            var imagesName = Directory
+                .EnumerateFiles(filePath)
+                .Select(Path.GetFileName)
+                .ToList();
+
+            return View(imagesName);
         }
 
         public IActionResult Privacy()
